404caa9cc452e2e4eab34ca80c04beb27beabfe6
biddulboatr <itohtristan@gmail.com>
2024-05-11 15:34:14 -0700

    change to other rooms/buildings

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -21,7 +21,6 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
-	print(direction)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
