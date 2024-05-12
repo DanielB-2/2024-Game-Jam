308d23a87825cf12a1a16f7e665ccbd7aa6498e6
NoahKYi <156741902+NoahKYi@users.noreply.github.com>
2024-05-11 20:17:42 -0700

    Just some stuffff
    Some stuffff

--- Player.gd
+++ Player.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 
 var speed = 500.0
 const JUMP_VELOCITY = -400.0
+<<<<<<< Updated upstream
+=======
+
+var Crouch = SPEED
+>>>>>>> Stashed changes
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -15,6 +20,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+<<<<<<< Updated upstream
 		
 	if Input.is_action_pressed("crouchKey"):
 		$CrouchingCollisionShape.disabled = false
@@ -41,9 +47,22 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 
+=======
+	
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+func _input(Input):
+	
+	if Input is InputEventKey and Input.pressed:
+		if Input.keycode == KEY_SHIFT:
+			print("Shift was pressed")
+			
+>>>>>>> Stashed changes
 			
 
 	var direction = Input.get_axis("left", "right")
+	
 	print(direction)
 	
 	if direction:
