bdda8d41fe2058049593a27b52a3d82a0d6ec591
Daniel Briggs <dtbriggs123@gmail.com>
2024-05-12 20:01:47 -0700

    started work on the guard

--- scenes/guard.gd
+++ scenes/guard.gd
@@ -1,11 +1,14 @@
-extends CollisionShape2D
+extends RigidBody2D
 
-player = get_node("/root/Player")
+var player
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = get_parent().get_node("Player")
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print($Player.position)
+	self.linear_velocity = self.position.direction_to(player.position) * 5
+	print(self.linear_velocity)
