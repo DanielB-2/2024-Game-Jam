10ad66e9b785639d4868f4d3d792e0b9f52bbb4b
Daniel Briggs <dtbriggs123@gmail.com>
2024-05-14 20:22:53 -0700

    trying to get enermies to follow the player (acctually added changes this time)

--- scenes/guard.gd
+++ scenes/guard.gd
@@ -5,10 +5,12 @@ var player
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_parent().get_node("Player")
+	
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.linear_velocity = self.position.direction_to(player.position) * 5
-	print(self.linear_velocity)
+	apply_central_force(self.position.direction_to(player.position) * 100)
+	print(self.position, player.position, self.position.direction_to(player.position) * 100)
+
