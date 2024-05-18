2c42167c4854394b2873941a9245ca59039d38e8
biddulboatr <itohtristan@gmail.com>
2024-05-17 21:27:23 -0700

    added intro

--- scripts/menuPlayButton.gd
+++ scripts/menuPlayButton.gd
@@ -6,4 +6,5 @@ func _ready():
 	pressed.connect(self._button_pressed)
 
 func _button_pressed():
-	get_tree().change_scene_to_file("res://scenes/node_3d.tscn")
+	get_tree().change_scene_to_file("res://scenes/Intro.tscn")
+	Global.introSequence()
