32c6779216dbbbb210f6d106b39e29dc9667295e
biddulboatr <itohtristan@gmail.com>
2024-05-18 13:38:05 -0700

    i forgot

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -16,10 +16,9 @@ extends CharacterBody2D
 var speed = 500.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Access the PlayerData singleton
+# singleton
 var playerData = PlayerData
 
 func _ready():
