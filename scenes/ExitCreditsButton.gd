67d8c71f697fd8700427a25d28c7f97491c970e9
Zander Coffman <zandergalvez330@gmail.com>
2024-05-19 12:44:00 -0700

    add

--- scenes/ExitCreditsButton.gd
+++ scenes/ExitCreditsButton.gd
@@ -1,15 +1,13 @@
 extends Button
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 
-func _button_pressed():
+func _pressed():
 	get_tree().change_scene_to_file("res://scenes/mainMenu.tscn")
